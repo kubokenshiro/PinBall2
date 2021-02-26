29902e906141a777b8703775ef6a004b7324464c
kubokenshiro <77647373+kubokenshiro@users.noreply.github.com>
2021-02-26 17:53:33 +0900

    課題変更１回目
    スコアの表示に着手。未完成。

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -6,12 +6,18 @@ using UnityEngine.UI;
 public class BallController : MonoBehaviour
 {
     private float visiblePosZ = -6.5f;
-
     private GameObject gameoverText;
+
+    //課題変更点
+    private int score = 0;
+    private GameObject scoreText;
 
     void Start()
     {
         this.gameoverText = GameObject.Find("GameOverText");
+
+        //課題変更点
+        this.scoreText = GameObject.Find("ScoreText");
     }
 
     void Update()
@@ -20,5 +26,23 @@ public class BallController : MonoBehaviour
         {
             this.gameoverText.GetComponent<Text>().text = "Game Over";
         }
+    }
+
+    //課題変更点
+    void OnCollisionEnter(Collision collision)
+    {
+        if(tag == "SmallStarTag")
+        {
+            this.score += 10;
+        }
+        else if(tag == "LargeStarTag")
+        {
+            this.score += 20;
+        }
+        else if(tag == "LargeCloudTag" || tag == "SmallCloudTag")
+        {
+            this.score += 30;
+        }
+        this.scoreText.GetComponent<Text>().text = "Score " + score;
     }
 }
